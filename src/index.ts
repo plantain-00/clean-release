8e3a471f1dac522c05a0e58b48d0207d507a35aa
york yao <yaoao12306@outlook.com>
2018-02-02 08:17:12 +0800

    feat: support prerelease

--- src/index.ts
+++ src/index.ts
@@ -92,11 +92,28 @@ async function executeCommandLine () {
   }
 
   if (configData.askVersion) {
+    let newVersionAnswer = await inquirer.prompt({
+      type: 'list',
+      name: 'identifier',
+      message: 'Select a new identifier:',
+      choices: [
+        '',
+        'alpha',
+        'beta',
+        'rc'
+      ]
+    })
+    const identifier = newVersionAnswer.identifier
+
     const patchVersion = semver.inc(packageJsonData.version, 'patch')!
     const minorVersion = semver.inc(packageJsonData.version, 'minor')!
     const majorVersion = semver.inc(packageJsonData.version, 'major')!
+    const prepatchVersion = semver.inc(packageJsonData.version, 'prepatch', true, identifier)!
+    const preminorVersion = semver.inc(packageJsonData.version, 'preminor', true, identifier)!
+    const premajorVersion = semver.inc(packageJsonData.version, 'premajor', true, identifier)!
+    const prereleaseVersion = semver.inc(packageJsonData.version, 'prerelease', true, identifier)!
     const customVersionChoice = 'Custom'
-    let newVersionAnswer = await inquirer.prompt({
+    newVersionAnswer = await inquirer.prompt({
       type: 'list',
       name: 'newVersion',
       message: 'Select a new version:',
@@ -112,6 +129,22 @@ async function executeCommandLine () {
         {
           name: `Major ${packageJsonData.version} -> ${majorVersion}`,
           value: majorVersion
+        },
+        {
+          name: `Pre Patch ${packageJsonData.version} -> ${prepatchVersion}`,
+          value: prepatchVersion
+        },
+        {
+          name: `Pre Minor ${packageJsonData.version} -> ${preminorVersion}`,
+          value: preminorVersion
+        },
+        {
+          name: `Pre Major ${packageJsonData.version} -> ${premajorVersion}`,
+          value: premajorVersion
+        },
+        {
+          name: `Pre Release ${packageJsonData.version} -> ${prereleaseVersion}`,
+          value: prereleaseVersion
         },
         customVersionChoice
       ]
