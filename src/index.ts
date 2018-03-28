88576cbb6a43c81d8d879e1dbabad1e4d6d3bf95
york yao <yaoao12306@outlook.com>
2018-03-28 20:59:47 +0800

    feat: move askVersion to npm-version-cli

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,7 @@ import cpy from 'cpy'
 import mkdirp from 'mkdirp'
 import * as childProcess from 'child_process'
 import * as rimraf from 'rimraf'
-import * as inquirer from 'inquirer'
-import * as semver from 'semver'
+import { askVersion } from 'npm-version-cli'
 import * as packageJson from '../package.json'
 
 function showToolVersion () {
@@ -34,18 +33,6 @@ function mkdirpAsync (dir: string) {
         reject(error)
       } else {
         resolve(made)
-      }
-    })
-  })
-}
-
-function writeFile (filename: string, data: string) {
-  return new Promise<void>((resolve, reject) => {
-    fs.writeFile(filename, data, error => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve()
       }
     })
   })
@@ -92,74 +79,7 @@ async function executeCommandLine () {
   }
 
   if (configData.askVersion) {
-    let identifierAnswer = await inquirer.prompt<{ identifier: string }>({
-      type: 'list',
-      name: 'identifier',
-      message: 'Select a new identifier:',
-      choices: [
-        '',
-        'alpha',
-        'beta',
-        'rc'
-      ]
-    })
-    const identifier = identifierAnswer.identifier
-
-    const patchVersion = semver.inc(packageJsonData.version, 'patch')!
-    const minorVersion = semver.inc(packageJsonData.version, 'minor')!
-    const majorVersion = semver.inc(packageJsonData.version, 'major')!
-    const prepatchVersion = semver.inc(packageJsonData.version, 'prepatch', true, identifier)!
-    const preminorVersion = semver.inc(packageJsonData.version, 'preminor', true, identifier)!
-    const premajorVersion = semver.inc(packageJsonData.version, 'premajor', true, identifier)!
-    const prereleaseVersion = semver.inc(packageJsonData.version, 'prerelease', true, identifier)!
-    const customVersionChoice = 'Custom'
-    let newVersionAnswer = await inquirer.prompt<{ newVersion: string }>({
-      type: 'list',
-      name: 'newVersion',
-      message: 'Select a new version:',
-      choices: [
-        {
-          name: `Patch ${packageJsonData.version} -> ${patchVersion}`,
-          value: patchVersion
-        },
-        {
-          name: `Minor ${packageJsonData.version} -> ${minorVersion}`,
-          value: minorVersion
-        },
-        {
-          name: `Major ${packageJsonData.version} -> ${majorVersion}`,
-          value: majorVersion
-        },
-        {
-          name: `Pre Patch ${packageJsonData.version} -> ${prepatchVersion}`,
-          value: prepatchVersion
-        },
-        {
-          name: `Pre Minor ${packageJsonData.version} -> ${preminorVersion}`,
-          value: preminorVersion
-        },
-        {
-          name: `Pre Major ${packageJsonData.version} -> ${premajorVersion}`,
-          value: premajorVersion
-        },
-        {
-          name: `Pre Release ${packageJsonData.version} -> ${prereleaseVersion}`,
-          value: prereleaseVersion
-        },
-        customVersionChoice
-      ]
-    })
-    if (newVersionAnswer.newVersion === customVersionChoice) {
-      newVersionAnswer = await inquirer.prompt<{ newVersion: string }>({
-        type: 'input',
-        name: 'newVersion',
-        message: 'Enter a custom version:',
-        filter: (input: string) => semver.valid(input)!,
-        validate: input => input !== null || 'Must be a valid semver version'
-      })
-    }
-    packageJsonData.version = newVersionAnswer.newVersion
-    await writeFile(packageJsonPath, JSON.stringify(packageJsonData, null, 2) + '\n')
+    await askVersion()
   }
 
   const result = tmp.dirSync()
